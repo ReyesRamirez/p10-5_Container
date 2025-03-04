f927c4e52a07d64dd61106ef043744fcf8613016
REYES_ALFONSO_RAMIREZ_MARTINEZ <a.22308051281289@cbtis128.edu.mx>
2025-03-04 16:34:54 +0000

    container

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,65 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            "Ramirez Container",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.red,
+        ),
+        backgroundColor: Colors.white.withOpacity(0.9),
+        body: Center(
+          // Envuelve la Column en Center
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment
+                .center, // Asegura que el contenido esté alineado al centro
+            children: [
+              Text(
+                "Reyes Alfonso Ramirez Martinez", // Reemplaza con tu nombre
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center, // Asegura el centrado del texto
+              ),
+              SizedBox(height: 5),
+              Text(
+                "Mat:22308051281289", // Reemplaza con tu matrícula
+                style: TextStyle(fontSize: 18, color: Colors.grey[700]),
+                textAlign: TextAlign.center, // Asegura el centrado del texto
+              ),
+              SizedBox(height: 20),
+              Container(
+                width: 150,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.green.withOpacity(1),
+                      offset: Offset(-5, -5),
+                      blurRadius: 10,
+                    ),
+                    BoxShadow(
+                      color: Colors.blue.withOpacity(1),
+                      offset: Offset(5, 5),
+                      blurRadius: 10,
+                    ),
+                  ],
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
